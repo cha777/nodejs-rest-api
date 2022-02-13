489214b61dbbc3c7471f8a858538f378786fd0e6
cha777 <chathuranga_wm@yahoo.com>
2022-02-13 20:03:09 +0530

    Boilerplate commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Sample NodeJS REST API with TypeScript
+
+This project is a sample REST API build with express.js and TypeScript to understand API basics.
+
+Use `yarn` to install the dependencies.
+
+Run `yarn start` to run the REST API locally (default port is configured to 4000).
+
+## Reference
+
+- Topal - Building Node.js/ TypeScript REST API - https://www.toptal.com/express-js/nodejs-typescript-rest-api-pt-1
---
+++ src/app.ts
@@ -0,0 +1,54 @@
+import express, { Application, Request, Response, json } from "express";
+import { createServer } from "http";
+import type { Server } from "http";
+
+import { format, transports } from "winston";
+import { logger } from "express-winston";
+import type { LoggerOptions } from "express-winston";
+
+import cors from "cors";
+import debug from "debug";
+import type { IDebugger } from "debug";
+
+import { CommonRoutesConfig } from "./common/common.routes.config";
+import { UsersRoutes } from "./users/users.routes.config";
+
+const app: Application = express();
+const server: Server = createServer(app);
+const port = 4000;
+const routes: Array<CommonRoutesConfig> = [];
+const debugLog: IDebugger = debug("app");
+
+app.use(json());
+app.use(cors());
+
+const loggerOptions: LoggerOptions = {
+  transports: [new transports.Console()],
+  format: format.combine(
+    format.json(),
+    format.prettyPrint(),
+    format.colorize({ all: true })
+  ),
+};
+
+if (!process.env.DEBUG) {
+  loggerOptions.meta = false;
+}
+
+app.use(logger(loggerOptions));
+
+routes.push(new UsersRoutes(app));
+
+const runningMessage = `Server running at http://localhost:${port}`;
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send(runningMessage);
+});
+
+server.listen(port, () => {
+  routes.forEach((route: CommonRoutesConfig) => {
+    debugLog(`Routes configured for ${route.getName()}`);
+  });
+
+  console.log(runningMessage);
+});
